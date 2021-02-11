375fa965183c68a164494ee2e88ab0438736de53
chetin4ever <hetan009@gmail.com>
2021-02-11 16:22:24 +0530

    init and creating youtube api using it in our app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <h1>hello</h1>
+    <search-bar @search="onTermChange" />
   </div>
-  <router-view/>
+  <router-view />
 </template>
+<script>
+import searchBar from "./components/searchBar.vue";
+import axios from "axios";
+const API_KEY = "AIzaSyApmpR5_NE80PE5H_3yChES4STEIYWsRQY";
+export default {
+  name: "App",
+  components: { searchBar },
+
+  setup() {
+    function onTermChange(serachTerm) {
+      axios
+        .get("https://www.googleapis.com/youtube/v3/search", {
+          params: {
+            key: API_KEY,
+            type: "video",
+            part: "snippet",
+            q: serachTerm,
+          },
+        })
+        .then((response) => console.log(response));
+    }
+    return { onTermChange };
+  },
+};
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/searchBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>search bar</h1>
+  <h2>{{ searchQuery }}</h2>
+  <input type="text" v-model="searchQuery" @input="onInputChange" />
+</template>
+
+<script>
+import { computed, ref } from "vue";
+export default {
+  setup(_, { emit }) {
+    const searchQuery = ref("");
+    console.log("helo");
+    function onInputChange() {
+      emit("search", this.searchQuery);
+    }
+
+    return { searchQuery, onInputChange };
+  },
+};
+</script>
+
+<style>
+</style>
